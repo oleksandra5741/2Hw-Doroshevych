56e4da4a713bbeadb2b3e84049ba71b048de9abb
oleksandra5741 <32238817+oleksandra5741@users.noreply.github.com>
2017-12-15 16:55:36 +0200

    Update LocalStorage.js

--- JS-Pizza-master/Frontend/src/LocalStorage.js
+++ JS-Pizza-master/Frontend/src/LocalStorage.js
@@ -1,9 +1,6 @@
 var basil	=	require('basil.js');
 basil	=	new	basil();
-
 exports.get =	function(key)	{
-    return	basil.get(key);
-};  
+    return	basil.get(key);};  
 exports.set =	function(key,	value)	{
-    return	basil.set(key,	value);
-};
+    return	basil.set(key,	value);};
