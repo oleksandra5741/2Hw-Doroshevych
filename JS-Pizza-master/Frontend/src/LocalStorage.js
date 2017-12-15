b49b3d7deaf2aa3fd21ffa4a61ec35fe2527eaf7
oleksandra5741 <32238817+oleksandra5741@users.noreply.github.com>
2017-12-15 17:57:12 +0200

    Delete LocalStorage.js

--- JS-Pizza-master/Frontend/src/LocalStorage.js
+++
@@ -1,6 +0,0 @@
-var base	=	require('base.js');
-base	=	new	base();
-exports.set =	function(key,value)	{
-    return	base.set(key,	value);};
-exports.get =	function(key){
-    return	basil.get(key);};
